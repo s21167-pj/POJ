51e4ef92b45cc42551e886e71567c04a0f558f54
s21167-pj <s21167@pjwstk.edu.pl>
2020-02-23 21:52:34 +0100

    Code reformat

---
+++ BMI.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class BMI {
+
+    public static void main(String[] args) {
+
+        Scanner in = new Scanner(System.in);
+
+
+        System.out.print("Enter weight in kg: ");
+        int kg = in.nextInt();
+
+
+        System.out.print("Enter height in m: ");
+        int m = in.nextInt();
+
+        double weightInKilos = kg;
+        double heightInMeters = (m * 0.01);
+        double bmi = weightInKilos / (heightInMeters * heightInMeters);
+
+
+        System.out.println("Your BMI is: " + bmi);
+
+        if (bmi < 18.5) {
+            System.out.print("Underweight");
+        } else if (bmi >= 18.5 && bmi < 25) {
+            System.out.print("Normal");
+        } else if (bmi >= 25 && bmi < 30) {
+            System.out.print("Overweight");
+        } else if (bmi >= 30) {
+            System.out.print("Obese");
+        }
+    }
+
+}
