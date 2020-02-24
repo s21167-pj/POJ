1aaa3d878a2db949627f44881bdbc4fc53e8aafe
s21167-pj <s21167@pjwstk.edu.pl>
2020-02-24 17:05:47 +0100

    Adding comments

--- BMI.java
+++ BMI.java
@@ -1,23 +1,22 @@
-import java.util.Scanner;
+import java.util.Scanner; // Import Scanner class for get user input
 
-public class BMI {
+public class BMI {  //
 
     public static void main(String[] args) {
 
-        Scanner in = new Scanner(System.in);
+        Scanner in = new Scanner(System.in); // Create a scanner object
 
-        System.out.print("Enter weight in kg: ");
-        int weight = in.nextInt();
+        System.out.print("Enter weight in kg: "); // Prompt user for enter weight kg
+        int weight = in.nextInt(); // Initalization of weight variable, and read user input
 
+        System.out.print("Enter height in cm: "); // Prompt user for enter height in cm
+        int heightInCentimetre = in.nextInt(); //  Initalization of height variable, and read user input
 
-        System.out.print("Enter height in cm: ");
-        int heightInCentimetre = in.nextInt();
+        double heightInMeters = (heightInCentimetre * 0.01); // Convert height to metres
+        double bmi = weight / (heightInMeters * heightInMeters); // Calculate BMI
 
-        double heightInMeters = (heightInCentimetre * 0.01);
-        double bmi = weight / (heightInMeters * heightInMeters);
-
-        System.out.println("Your BMI is: " + bmi);
-
+        System.out.println("Your BMI is: " + bmi); // Output BMI
+        // simple if checking bmi between specific periods
         if (bmi < 18.5) {
             System.out.print("Underweight");
         } else if (bmi >= 18.5 && bmi < 25) {
