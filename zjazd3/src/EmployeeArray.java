7b03ee98900f8277080a80c7799da448083acfd1
Marcel Pankanin <s21167@pjwstk.edu.pl>
2020-03-28 21:08:54 +0100

    Employee array task

---
+++ zjazd3/src/EmployeeArray.java
@@ -0,0 +1,54 @@
+/*
+Employee array list implemented in Java.
+Author: Marcel P
+ */
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+public class EmployeeArray {
+    private String name;
+    private int salary;
+    private int joinDate;
+
+    public EmployeeArray(String name, int salary, int joinDate) {
+        this.name = name;
+        this.salary = salary;
+        this.joinDate = joinDate;
+    }
+
+    public EmployeeArray() {
+
+    }
+
+    static void fillList() {
+        String[] namesToRandom = {"Kris", "Bob", "Franklin", "Mark", "Kate", "Ashley",
+                "Penelope", "Antonio", "Victor", "Edward", "Randy"};
+        List<EmployeeArray> employeeAll = new ArrayList<EmployeeArray>();
+        int numOfEmployees = 8;
+        int lowYear = 1990;
+        int highYear = 2020;
+        Random year = new Random();
+        for (int i = 0; i < numOfEmployees; i++) {
+            EmployeeArray temp = new EmployeeArray();
+            temp.name = namesToRandom[(new Random()).nextInt(namesToRandom.length)];
+            temp.salary = (int) (Math.random() * 1500 + 10000);
+            temp.joinDate = year.nextInt(highYear - lowYear) + lowYear;
+            employeeAll.add(temp);
+        }
+        employeeAll.forEach(System.out::println);
+    }
+
+    public static void main(String[] args) {
+        fillList();
+    }
+
+    @Override
+    public String toString() {
+        return
+                "name = " + name +
+                        ", salary = " + salary +
+                        ", joinDate = " + joinDate;
+    }
+}
---
+++ zjazd3/src/Time.java
@@ -0,0 +1,93 @@
+/*
+Time UML chart implemented in Java.
+Author: Marcel P
+ */
+public class Time {
+    private int hour;
+    private int minute;
+    private int second;
+
+    public Time(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    public static void main(String[] args) {
+        Time time = new Time(23, 59, 59);
+        //  time.toString();
+
+    }
+
+    public int getHour() {
+        return hour;
+    }
+
+    public void setHour(int hour) {
+        this.hour = hour;
+    }
+
+    public int getMinute() {
+        return minute;
+    }
+
+    public void setMinute(int minute) {
+        this.minute = minute;
+    }
+
+    public int getSecond() {
+        return second;
+    }
+
+    public void setSecond(int second) {
+        this.second = second;
+    }
+
+    public void setTime(int hour, int minute, int second) {
+        this.hour = hour;
+        this.minute = minute;
+        this.second = second;
+    }
+
+    @Override
+    public String toString() {
+        String formattedHour = String.format("%02d", hour);
+        String formattedMinute = String.format("%02d", minute);
+        String formattedSecond = String.format("%02d", second);
+        return String.format("%s:%s:%s", formattedHour, formattedMinute, formattedSecond);
+    }
+
+    public Time nextSecond() {
+        if (second == 59) {
+            second = 0;
+            if (minute == 59) {
+                minute = 0;
+                if (hour == 23)
+                    hour = 0;
+                else
+                    hour += 1;
+            } else {
+                minute += 1;
+            }
+        } else
+            second += 1;
+        return this;
+    }
+
+    public Time previousSecond() {
+        if (second == 0) {
+            second = 59;
+            if (minute == 0) {
+                minute = 59;
+                if (hour == 0)
+                    hour = 23;
+                else
+                    hour -= 1;
+            } else {
+                minute -= 1;
+            }
+        } else
+            second -= 1;
+        return this;
+    }
+}
