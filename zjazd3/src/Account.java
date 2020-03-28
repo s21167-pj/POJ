34351e837be5726ebe489f8db6187a629b473eb3
Marcel Pankanin <s21167@pjwstk.edu.pl>
2020-03-28 12:28:26 +0100

    Acc, invoice, employee uml

---
+++ zjazd3/src/Account.java
@@ -0,0 +1,76 @@
+/*
+Account UML chart implemented in Java.
+Author: Marcel P
+ */
+public class Account {
+    private String id;
+    private String name;
+    private int balance = 0;
+
+    public Account(String id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Account(String id, String name, int balance) {
+        this.id = id;
+        this.name = name;
+        this.balance = balance;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public int credit(int amount) {
+        System.out.println(balance = balance + amount);
+        return balance = balance + amount;
+    }
+
+    public int debit(int amount) {
+        if (amount <= balance)
+            balance = balance - amount;
+        else
+            System.out.println("Amount exceeded balance");
+        System.out.println(balance);
+        return balance;
+    }
+
+    public int transferTo(Account another, int amount) {
+        if (amount <= balance)
+            balance = balance - amount;
+        another.balance += another.balance + amount;
+        System.out.println(another.balance);
+        //else
+        //  System.out.println("Amount exceeded balance");
+        // System.out.println(balance);
+        return balance;
+    }
+
+    @Override
+    public String toString() {
+        return "Account{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", balance=" + balance +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Account kuba = new Account("1", "kubowe", 100);
+        Account bartek = new Account("2", "bartkowe", 50);
+
+        kuba.credit(1500);
+        bartek.debit(50);
+        kuba.transferTo(bartek, 1);
+    }
+
+}
---
+++ zjazd3/src/InvoiceItem.java
@@ -0,0 +1,62 @@
+/*
+Invoice UML chart implemented in Java.
+Author: Marcel P
+ */
+public class InvoiceItem {
+    private int id;
+    private String desc;
+    private int qty;
+    private double unitPrice;
+
+    public InvoiceItem(int id, String desc, int qty, double unitPrice) {
+        this.id = id;
+        this.desc = desc;
+        this.qty = qty;
+        this.unitPrice = unitPrice;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getDesc() {
+        return desc;
+    }
+
+    public int getQty() {
+        return qty;
+    }
+
+    public void setQty(int qty) {
+        this.qty = qty;
+    }
+
+    public double getUnitPrice() {
+        return unitPrice;
+    }
+
+    public void setUnitPrice(double unitPrice) {
+        this.unitPrice = unitPrice;
+    }
+    public double getTotal(){
+        double total = unitPrice * qty;
+        System.out.println(total);
+        return unitPrice * qty;
+    }
+
+    @Override
+    public String toString() {
+        return "InvoiceItem{" +
+                "id=" + id +
+                ", desc='" + desc + '\'' +
+                ", qty=" + qty +
+                ", unitPrice=" + unitPrice +
+                '}';
+    }
+
+    public static void main(String[] args) {
+    InvoiceItem Maciek = new InvoiceItem(1, "balony", 50000, 2.50);
+    Maciek.getTotal();
+
+    }
+}
